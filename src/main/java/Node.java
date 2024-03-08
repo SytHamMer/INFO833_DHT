00102fa7049847d41dfa95c9bb09ed8c6e999d25
SytHamMer <mathys.lebon1@gmail.com>
2024-03-08 08:21:35 +0100

    [EDIT]: send function attributs

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -78,7 +78,7 @@ public class Node {
                         //Send ack to the old leftNeighbor
                         Message ack = new Message("join", "ack", this.getInfNeighbor().get(0), null);
                         //Need to know how to access to events
-                        this.send(ack, this.getSupNeighbor().get(0), dht.getEvents(), dht);
+                        this.send(ack, this.getSupNeighbor().get(0), dht);
                         //change the infNeighbor of the receiver node
                         node.setInfNeighbor(message.getSenders().get(0), message.getSenders().get(1));
 
@@ -136,12 +136,12 @@ public class Node {
     }
 
     //génére l'event + calcul latence
-    public void send(Message message, int id, PriorityQueue<Evenement> events, DHT dht) {
+    public void send(Message message, int id,  DHT dht) {
         Random rand = new Random();
         //Latence aléatoire entre 10 et 50 ms amélioration possible (vrai calcul et non random)
         int latence = rand.nextInt(50 - 10 + 1) + 10;
         int executeTime = latence + dht.getCurrentTime();
-        events.add(new Evenement(executeTime, id, message,dht));
+        dht.getEvents().add(new Evenement(executeTime, id, message,dht));
 
 
     }
