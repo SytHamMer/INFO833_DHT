3ad22a466850487ca8d9c80862958c5b2974cd9e
lilcdx <lila.cadoux@gmail.com>
2024-03-08 09:59:43 +0100

    [ADD]: joinRequest function

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -58,38 +58,23 @@ public class Node {
         return loc;
     }
 
-    public Node getById(int id){
-        return this;
-    }
-
-    public Node getByLoc(int loc){
-        return this;
-    }
-
 
     // for now easy version always go up but could be improved by taking the shortest way
     public void deliver(Message message){
         switch (message.getType()){
             case "join":
+                Node newNode = dht.getNodeById(message.getSenders().get(0));
                 switch (message.getSousType()){
                     case "insert":
                         System.out.println("join_insert");
-                        Node node = this.getById( this.getSupNeighbor().get(0));
-                        //Send ack to the old leftNeighbor
-                        Message ack = new Message("join", "ack", this.getInfNeighbor().get(0), null);
-                        //Need to know how to access to events
-                        this.send(ack, this.getSupNeighbor().get(0), dht);
-                        //change the infNeighbor of the receiver node
-                        node.setInfNeighbor(message.getSenders().get(0), message.getSenders().get(1));
 
 
                         break;
                     case "request":
                         System.out.println("join_request");
-
-
-
+                        joinRequest(newNode, message);
                         break;
+
                     case "ack":
                         System.out.println("join_ack");
                         break;
@@ -136,17 +121,44 @@ public class Node {
     }
 
     //génére l'event + calcul latence
-    public void send(Message message, int id,  DHT dht) {
+    public void send(Message message, int receiverId) {
         Random rand = new Random();
         //Latence aléatoire entre 10 et 50 ms amélioration possible (vrai calcul et non random)
         int latence = rand.nextInt(50 - 10 + 1) + 10;
         int executeTime = latence + dht.getCurrentTime();
-        dht.getEvents().add(new Evenement(executeTime, id, message,dht));
+        dht.getEvents().add(new Evenement(executeTime, receiverId, message,dht));
 
 
     }
 
+    public void joinRequest(Node newNode, Message m){
+        int newLoc = newNode.getLoc();
+        int newId = newNode.getId();
+        if(newLoc>loc){
+            int supNeighborLoc = supNeighbor.get(1);
+            if (supNeighborLoc>newLoc || loc> supNeighborLoc){
+                HashMap<String, String> mData = new HashMap<>();
+                mData.put("insertIdNode", Integer.toString(newId));
+                send(new Message("join", "insert", id, mData), supNeighbor.get(0));
 
+            } else {
+                m.addSender(id);
+                send(m, supNeighbor.get(0));
+            }
+        } else {
+            int infNeighborLoc = infNeighbor.get(1);
+            if (infNeighborLoc<newLoc || loc<infNeighborLoc){
+                HashMap<String, String> mData = new HashMap<>();
+                mData.put("insertIdNode", Integer.toString(newId));
+                send(new Message("join", "insert", id, mData), infNeighbor.get(0));
+
+            } else {
+                m.addSender(id);
+                send(m, supNeighbor.get(0));
+            }
+        }
+
+    }
 
 
 }
